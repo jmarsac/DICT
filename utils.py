622d26bfd188562dd32cd1288f9c0a57249b496a
jmarsac <jmarsac@azimut.fr>
2020-07-24 18:36:23 +0200

    Add functions for variables expanding in string - stringContainsVariable to check string fro variables - expandVariablesInString() to replace variables by their values

--- utils.py
+++ utils.py
@@ -19,6 +19,11 @@
  *                                                                         *
  ***************************************************************************/
 """
+from PyQt5.QtCore import (QSettings, QTranslator, qVersion,
+                          QCoreApplication, QDir, QFileInfo,
+                          QFile, Qt, QUrl)
+from qgis.core import QgsApplication, QgsSettings, QgsProject, QgsExpressionContextUtils, QgsExpressionContext, QgsExpression
+from qgis.PyQt.QtCore import QRegularExpression, QRegularExpressionMatch
 import os
 
 class Utils(object):
@@ -28,6 +33,27 @@ class Utils(object):
         """Constructor."""
         print('__init__')
 
+    @classmethod
+    def stringContainsVariable(cls, str_to_check):
+        regex = QRegularExpression("@[a-zA-Z][a-zA-Z0-9_]*")
+        return regex.match(str_to_check).hasMatch()
+
+    @classmethod
+    def expandVariablesInString(cls, str_to_expand, use_system_separator:bool=False):
+        regex = QRegularExpression("@[a-zA-Z][a-zA-Z0-9_]*")
+        if regex.match(str_to_expand).hasMatch():
+            context = QgsExpressionContext()
+            context.appendScope(QgsExpressionContextUtils.globalScope())
+            context.appendScope(QgsExpressionContextUtils.projectScope(QgsProject.instance()))
+            iter = regex.globalMatch(str_to_expand)
+            while iter.hasNext():
+                match = iter.next()
+                if context.hasVariable(match.captured()[1:]):
+                    str_to_expand = str_to_expand.replace(match.captured(), context.variable(match.captured()[1:]))
+
+            return QDir.toNativeSeparators(str_to_expand) if use_system_separator else str_to_expand
+        else:
+            return str_to_expand
 
     @classmethod
     def resolve(cls, name, basepath=None):
